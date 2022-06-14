62ea53a454b040021e40784df632e28b4b408517
Julien Armagnac <julien.tamil@gmail.com>
2022-06-14 19:54:07 +0200

    Debug home page, view Users#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :carts
   resources :orders
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
