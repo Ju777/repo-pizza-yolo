a2d3d56b661295c1f3760909a047081b4bbea22c
Julien Armagnac <julien.tamil@gmail.com>
2022-06-14 11:30:38 +0200

    Finish datas validations + add automatic Cart creation after a User creation.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,16 @@ class Restaurant < ApplicationRecord
   belongs_to :manager, class_name: "User"
   has_many :product_restaurants
   has_many :products, through: :product_restaurants
+
+  validates :name, :street, :city,
+    presence: true,
+    length: { maximum: 30 }
+
+  validates :zipcode,
+    presence: true,
+    format: { with: /\A(?:0[1-9]|[1-8]\d|9[0-8])\d{3}\z/ }
+    
+  validates :phone,
+    presence: true,
+    format: {with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/}
 end
