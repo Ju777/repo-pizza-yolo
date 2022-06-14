fb5e6aa2fdea7b4227f0f10dfd49fe61b2b08580
0xKubitus <n.cubilier@gmail.com>
2022-06-14 14:23:03 +0200

    updated environment.rb with config SMTP

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILJET_LOGIN'],
+  :password => ENV['MAILJET_PWD'],
+  :domain => 'pizza-yolo.fr',
+  :address => 'in-v3.mailjet.com',
+  :tls => true,
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
