c669691a6797e7dae1cf8b054f170927bb45e19e
0xKubitus <n.cubilier@gmail.com>
2022-06-14 15:23:51 +0200

    added method in 'user_mailer' for order_recap_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,24 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'https://git.heroku.com/pizza-yolo.git/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
+  def order_recap_email(user)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    @user = user 
+
+    #on récupère l'instance de la commande en question
+    @order = user.order.last
+
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'https://git.heroku.com/pizza-yolo.git/' 
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @user.email, subject: 'PIZZA-YOLO: Récapitulatif de votre commande') 
+  end
+
 end
