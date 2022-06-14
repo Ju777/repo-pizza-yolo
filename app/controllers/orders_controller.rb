f985c3e14e1482a8edb9e35ef8b0ec47b5a7adbf
Julien Armagnac <julien.tamil@gmail.com>
2022-06-14 14:38:06 +0200

    Add Confirm Order button in Cart#Show view.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    puts "#"*100
+    puts "On est dans la méthode NEW de Orders."
+    puts "#"*100
+  end
+
+  def create
+    puts "#"*100
+    puts "On est dans la méthode CREATE de Orders."
+    puts "#"*100
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,38 +1,38 @@
 require 'test_helper'
 
-class CartControllerTest < ActionDispatch::IntegrationTest
+class CartsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get cart_index_url
+    get carts_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get cart_show_url
+    get carts_show_url
     assert_response :success
   end
 
   test "should get new" do
-    get cart_new_url
+    get carts_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get cart_create_url
+    get carts_create_url
     assert_response :success
   end
 
   test "should get edit" do
-    get cart_edit_url
+    get carts_edit_url
     assert_response :success
   end
 
   test "should get update" do
-    get cart_update_url
+    get carts_update_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get cart_destroy_url
+    get carts_destroy_url
     assert_response :success
   end
 
