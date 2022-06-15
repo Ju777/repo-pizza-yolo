330edf648f7e46a84b72f9b4b600025adb292dca
Julien Armagnac <julien.tamil@gmail.com>
2022-06-15 12:23:25 +0200

    Add DB relation Orders <-> Restaurant.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   after_create :order_recap
   
   belongs_to :user
+  belongs_to :restaurant
 
   def order_recap
     UserMailer.order_recap_email(self).deliver_now
