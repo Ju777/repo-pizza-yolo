d4443a2d0889d70e4ea9cbea0c807d66bf005518
0xKubitus <n.cubilier@gmail.com>
2022-06-15 11:44:05 +0200

    added: link to edit_user_path of current_user if profile isnt complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   resources :carts
   resources :orders
-  resources :users, only: [:show]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
