48280c3330d25e930101f1cce05e098415c547f9
KarineDHoshi <karindasilva33@yahoo.com>
2022-06-15 16:05:27 +0200

    add update to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user! 
+  before_action :set_user, only: [:show, :edit, :update, :destroy,]
   before_action :check_profile_owner, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -18,9 +19,36 @@ class UsersController < ApplicationController
       @user_orders = current_user.orders
       
   end
-  
+
+  def edit
+   @user = User.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_path(@user), notice: "Modifications enregistrées" }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :description, :firstname, :lastname, :phone,)
+  end
+
+
+  
   def check_profile_owner
     profile_owner = User.find(params[:id])
         
