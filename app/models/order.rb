8a6b21a8e53f94b8429871f8b420251aca8fd068
0xKubitus <n.cubilier@gmail.com>
2022-06-15 17:19:27 +0200

    tested and works fine locally - maybe we should try deploy this branch in dev environment of heroku to test it in production?

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
 
   private
 
-  def order_recap
+  def order_recap #method to send emails after an order is paid
     unless self.pickup_code == "not_paid"
       UserMailer.customer_order_email(self).deliver_now # email sent to customer
       UserMailer.pizzeria_order_email(self).deliver_now # email sent to restaurant's manager
