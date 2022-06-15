e34c75ad2490b78d9a6cf91621a1a24a25023785
Julien Armagnac <julien.tamil@gmail.com>
2022-06-15 07:52:33 +0200

    Comment some code in Users#show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
-    def index
-    end
-    def show
-        @user = current_user
-        @firstname = current_user.firstname
-        @lastname = current_user.lastname
-        @email = current_user.email
-        @phone = current_user.phone
+  before_action :authenticate_user! 
+  before_action :check_profile_owner, only: [:show, :edit, :update, :destroy]
 
-        @orders = current_user.orders.last
+  def index
+  end
+
+  def show
+    # (par Julien) Comme c'est directement le curren_user qui est appelé dans la view users#show, les variables suivantes ne servent qu'au controller.
+    # On devrait pouvoir les supprimer, elles ne sont pas appelées ailleurs pour l'instant.
+
+    # @user = current_user
+    # @firstname = current_user.firstname
+    # @lastname = current_user.lastname
+    # @email = current_user.email
+    # @phone = current_user.phone
+
+      @user_orders = current_user.orders
+      
+  end
+  
+  private
+
+  def check_profile_owner
+    profile_owner = User.find(params[:id])
         
+    unless current_user == profile_owner
+      flash.notice = "Accès non autorisé."
+      redirect_to root_path
     end
-    
+  end
+
 end
