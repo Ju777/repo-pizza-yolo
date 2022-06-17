de284f0da3c8dbce6b8e273be3bea42ce023f5d6
0xKubitus <n.cubilier@gmail.com>
2022-06-17 16:34:59 +0200

    1st commit

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -11,53 +11,83 @@ class CartProductsController < ApplicationController
   end
 
   def create
+
+    product = Product.find(params[:product_id])
+
+    cart_product = CartProduct.find_by(product: product, cart: current_user.cart)
+
+    unless current_user.cart.products.include?(product)
+      @cart_product = CartProduct.new(product: product, cart:current_user.cart, quantity:1)
+    else
+      @cart_product.update(quantity: cart_product.quantity+1)
+    end
+
+    if @cart_product.save!
+      flash[:success] = "Produit ajouté au panier"
+      redirect_to cart_path(current_user.cart)
+    else
+      flash[:error] = "Erreur d'ajout au panier"
+      redirect_to root_path
+    end
+
+
+
+
+=begin
+    #on identifie le produit à ajouter au panier via la table intermedaire CartProducts:
     product_to_add = Product.find(params[:product_id])
+
+    searched_cart_products = current_user.cart.cart_products.where(product: product_to_add)
+
     @new_cart_product = CartProduct.new(cart:current_user.cart, product: product_to_add)
     @new_cart_product.quantity = 1
+    @new_cart_product.save
+
+    if current_user.cart.cart_products.where(product: product_to_add).length > 0
+      puts " "
+      puts "#"*50
+      puts "le cart product existe"
+      puts "#"*50
+      puts " "
+    else
+      puts " "
+      puts "="*50
+      puts "le cart product n'existe pas"
+      puts "="*50
+    end
+
+
+    #Si ce produit n'est pas dans le panier, alors on l'y ajoute; sinon, ajouter +1 à la quantité du CartProduct en question :
+    if current_user.cart.products.include?(product_to_add)
+      product_to_add.update(quantity: searched_cart_products.quantity + 1) # <- il nous faut chercher la quantité non pas du product_to_add, mais du CartProduct déja existant... et je n'arrive pas à trouver comment faire pour identifier cette variable...
+    else
+      @new_cart_product = CartProduct.new(cart:current_user.cart, product: product_to_add)
+      @new_cart_product.quantity = 1
+    end
 
     if @new_cart_product.save
       flash.notice="Produit ajouté avec succès."
-      redirect_to products_path
+      redirect_to cart_path(current_user.cart)
     else
       flash.alert="Produit non ajouté. Erreur."
       redirect_to product_path(product_to_add)
     end
+
+=end
+
   end
+
 
   def edit
   end
 
   def update
-    #on identifie le produit selectionné dans le panier:
-    @cartproduct_to_update = CartProduct.find(params[:id])
-
-    puts " "
-    puts "="*50
-    puts "voici le cartproduct_to_update : #{@cartproduct_to_update}"
-    puts "="*50
-    puts " " 
-
-    puts " "
-    puts "="*50
-    puts "qté du cartproduct_to_update : #{@cartproduct_to_update.quantity}"
-    puts "="*50
-    puts " "
-
-    @cartproduct_to_update.quantity += 1 
-
-    puts " "
-    puts "="*50
-    puts "qté du cartproduct_to_update apres l'update : #{@cartproduct_to_update.quantity}"
-    puts "="*50
-    puts " "
-    
-    # on redirige vers le panier (1ère étape, ensuite il faudra que ça soit fait via AJAX)
-    redirect_to cart_path(current_user)
   end
 
   def destroy
     product = CartProduct.find(params[:id])
     product.destroy
+
     respond_to do |format|
       format.html {
         flash.notice = "Produit retiré du panier"
@@ -65,7 +95,6 @@ class CartProductsController < ApplicationController
       }
       format.js {}
     end
-
-
   end
+
 end
