e61746e025f9274be01d22694b2fa4d5a5dfb1ab
Julien Armagnac <julien.tamil@gmail.com>
2022-06-24 09:29:53 +0200

    Only code cleaning + indentation.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,45 +6,9 @@ class OrdersController < ApplicationController
   end
 
   def new
-    # Tout ce ceci est utilisé dans Schedule#new
-    # @cart_to_show = current_user.cart
-    # @pizzas_quantity = pizzas_to_cook
-    # @total_to_pay = total_cart
-    # @order = Order.create(total_amount: @total_to_pay, user: current_user, restaurant: Restaurant.first)
-    # @order.update(pickup_code: "#{@order.id}##{@order.created_at.to_i}")
   end
 
   def create
-    #############################################
-    # STRIPE V1 process begins
-    # Before the rescue, at the beginning of the method
-    # @current_order = Order.where(user:current_user).last
-    # @stripe_amount = ((@current_order.total_amount)*100).to_i
-
-    # begin
-    #   customer = Stripe::Customer.create({
-    #   email: params[:stripeEmail],
-    #   source: params[:stripeToken],
-    #   })
-    #   charge = Stripe::Charge.create({
-    #   customer: customer.id,
-    #   amount: @stripe_amount,
-    #   description: "Achat d'un produit",
-    #   currency: 'eur',
-    #   })
-    #   final_pickup_code = "#{@current_order.id}##{@current_order.created_at.to_i}"
-    #   @current_order.update(pickup_code:final_pickup_code)
-    #   flash.notice="Commande validée, vous recevrez votre code de retrait à l'adresse #{current_user.email}."
-    #   redirect_to root_path
-      
-    # rescue Stripe::CardError => e
-    #   flash[:error] = e.message 
-    #   redirect_to root_path
-    # end
-    # After the rescue, if the payment succeeded
-    # STRIPE V1 process ends
-    ############################################# 
-
     #############################################
     # STRIPE V2 process begins
     
@@ -79,25 +43,18 @@ class OrdersController < ApplicationController
 
   def destroy
     empty_cart
-    current_user.orders.last.destroy # Ajouté suite à relecture
-
+    current_user.orders.last.destroy
     flash.notice= "Panier vidé, commande annulée."
     redirect_to root_path    
   end
 
   def success
-    puts "#"*100
-    puts "Je suis la méthode success ! :) ."
-    puts "#"*100
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
     total_amount = params[:total]
     order = current_user.orders.last
     pickup_code = "#{order.id}##{order.created_at.to_i}"
     order.update(pickup_code:pickup_code)
-    puts "#"*100
-    puts "order = #{order.inspect}."
-    puts "#"*100
     empty_cart
     clean_old_schedules
     clean_wrong_orders
@@ -112,31 +69,14 @@ class OrdersController < ApplicationController
   private 
 
   def empty_cart
-    puts "#"*100
-    puts "Je suis la méthode empty_cart DEBUT ! :) ."
-    puts "#"*100
     cart_products_to_empty = current_user.cart.cart_products
     cart_products_to_empty.each do |cart_product|
-    puts "#"*100
-    puts "Je suis la méthode empty_cart EACH ! :) ."
-    puts "#"*100
       cart_product.destroy
     end
 
   end
 
   def clean_old_schedules
-    puts "#"*100
-    puts "Je suis la méthode clean_old_schedules DEBUT ! :) ."
-    puts "#"*100
-    # cart_products = current_user.cart.cart_products
-    # cart_products.each do |cart_product|
-    #   puts "#"*100
-    #   puts "Je suis la méthode clean_old_schedules DANS EACH ! :) ."
-    #   puts "#"*100
-    #   cart_product.schedule.destroy
-    # end
-
     Schedule.all.each do |schedule|
       if schedule.date.year == 1900 && schedule.created_at < Time.now - 3600*24
         schedule.destroy
@@ -146,9 +86,7 @@ class OrdersController < ApplicationController
 
   def clean_wrong_orders
     Order.where(pickup_code: nil).each do |order|
-      # if order.pickup_code == nil
         order.destroy
-      # end
     end
   end
 
